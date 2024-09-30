dac2afcbed6fe7f833e26caaa9b5fca8b12e9cba
achitoo <hbaltodano@uamv.edu.ni>
2024-09-30 10:23:12 -0600

    Sesion 13

---
+++ Metodo burbuja/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Metodo_burbuja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNumber = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lbNumbers = new System.Windows.Forms.ListBox();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese un #";
+            // 
+            // tbNumber
+            // 
+            this.tbNumber.Location = new System.Drawing.Point(134, 59);
+            this.tbNumber.Name = "tbNumber";
+            this.tbNumber.Size = new System.Drawing.Size(100, 22);
+            this.tbNumber.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(251, 18);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(115, 35);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lbNumbers
+            // 
+            this.lbNumbers.FormattingEnabled = true;
+            this.lbNumbers.ItemHeight = 16;
+            this.lbNumbers.Location = new System.Drawing.Point(47, 105);
+            this.lbNumbers.Name = "lbNumbers";
+            this.lbNumbers.Size = new System.Drawing.Size(319, 164);
+            this.lbNumbers.TabIndex = 3;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(251, 59);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(115, 32);
+            this.btnSort.TabIndex = 4;
+            this.btnSort.Text = "Ordenar";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 335);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.lbNumbers);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbNumber);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Metodo de burbuja";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNumber;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lbNumbers;
+        private System.Windows.Forms.Button btnSort;
+    }
+}
+
---
+++ Metodo burbuja/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Metodo_burbuja.Models;
+
+
+namespace Metodo_burbuja
+
+{
+    public partial class Form1 : Form
+    {
+        int[] numbers = new int[10];
+        int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int number = int.Parse(tbNumber.Text);
+                numbers[i++] = number;
+                ShowNumbers();
+            } catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
+        public void ShowNumbers()
+        {
+            if (lbNumbers.Items.Count > 0)
+            {
+                lbNumbers.Items.Clear();
+            }
+
+            foreach (int number in numbers)
+            {
+                lbNumbers.Items.Add(number);
+            }
+
+
+
+        }
+        private void Clear()
+        {
+            tbNumber.Clear();
+            lbNumbers.ClearSelected();
+
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            Bubble bubble = new Bubble();
+            bubble.SortBubble(numbers);
+            ShowNumbers();
+        }
+    }
+   
+    
+}
